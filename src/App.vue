3c2e6b9341dc78940d91c9e47e694393d30acf30
cm <1339279735@qq.com>
2019-08-03 14:52:39 +0800

    使用element-ui和vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
     APP
+    <el-button type="success">成功按钮</el-button>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
 </script>
 
 <style lang="less">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// import router from './router/index.js'
+// 上边这个可以简写为import router from '@/router'
+//  @是一个路径的别名,是src路径,在vue-cli项目下有效,index是目录的索引文件,默认使用的索引文件,index.js     index.vue    index.json
+import router from '@/router'
+Vue.use(ElementUI)
+// Vue.config.productionTip = false的意思是:是否开启产品级别提示,不开启
+// 开发阶段是false,上线时候是true
 Vue.config.productionTip = false
-
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
